943d978a1512ad9d8e7659ebd7faf33038769e3a
Alexandra Davis <alex.davis806@gmail.com>
2022-06-13 13:55:02 -0700

    styling

--- client/src/Components/ProductItem.js
+++ client/src/Components/ProductItem.js
@@ -1,32 +1,27 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
-const ProductItem = ( { product, user } ) => {
-    const { name, brand, ingredients, notes, image } = product
+const ProductItem = ({ product, user }) => {
+  const { name, brand, ingredients, notes, image } = product;
 
-    const handleDelete = (e) => {
-        fetch(`/users/${user.id}/products/${product.id}`, {
-        method: 'DELETE'
-        })
-        // deleteProduct(product)
-    }
+  const handleDelete = (e) => {
+    fetch(`/users/${user.id}/products/${product.id}`, {
+      method: "DELETE",
+    });
+    // deleteProduct(product)
+  };
 
-    const editProduct = () => {
-        console.log('edit this product!')
-    }
-    return (
-        <Card sx={{ maxWidth: '15rem' }}>
-      <CardMedia
-        component="img"
-        height="200"
-        image={image}
-        alt={name}
-      />
+  const editProduct = () => {
+    console.log("edit this product!");
+  };
+  return (
+      <>
+       <CardMedia component="img" height="200" image={image} alt={name} />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
           Product: {name}
@@ -39,15 +34,18 @@ const ProductItem = ( { product, user } ) => {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" onClick={editProduct}>Edit Product</Button>
-        <Button size="small" onClick={() => handleDelete()}>Delete Product</Button>
+        <Button size="small" onClick={editProduct}>
+          Edit Product
+        </Button>
+        <Button size="small" onClick={() => handleDelete()}>
+          Delete Product
+        </Button>
       </CardActions>
-    </Card>
-      
+      </>
     );
-}
+};
 
-export default ProductItem
+export default ProductItem;
 
 //    <Card variant="outlined">
 //             <h1>PRODUCT</h1>
