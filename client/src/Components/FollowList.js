47cd81ec3e8f83432b5b13fc550339996779d7c4
Alexandra Davis <alex.davis806@gmail.com>
2022-06-16 09:38:28 -0700

    followers list functional

--- client/src/Components/FollowList.js
+++ client/src/Components/FollowList.js
@@ -1,14 +1,51 @@
-const FollowList = ( { users } ) => {
-  
-  const userList = users.map((user) => {
-    return <li>{user.username}</li>
-  })
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+  Avatar,
+  Container,
+} from "@mui/material";
+
+const FollowList = ({ follow }) => {
+  console.log(follow);
+  let navigate = useNavigate();
+
+  const visitProfile = () => {
+    navigate(`/users/${follow.id}/profile`);
+  };
 
   return (
-    <ul>
-      {userList}
-    </ul>
-  )
+    <>
+      <Container maxWidth="md" sx={{ pt: 4 }}>
+        <Box>
+          <Card sx={{ maxWidth: 200 }}>
+            <CardContent>
+              <Avatar sx={{ width: 56, height: 56 }}>
+                {follow.username[0]}
+              </Avatar>
+              <Typography
+                sx={{ fontSize: 14 }}
+                color="text.secondary"
+                gutterBottom
+              ></Typography>
+              <Typography variant="h5" component="div">
+                {follow.username}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button onClick={visitProfile} variant="contained" size="small">
+                See Profile
+              </Button>
+            </CardActions>
+          </Card>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default FollowList;
---
+++ client/src/Components/Followers.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import FollowList from "./FollowList";
+
+const Followers = () => {
+    const [followers, setFollowers] = useState([]);
+
+    useEffect(() => {
+        fetch("/followers").then((r) => {
+          if (r.ok) {
+            r.json().then((followers) => setFollowers(followers));
+          }
+        });
+      }, []);
+      console.log(followers)
+
+      const followerList = followers.map((follower) => {
+        return <FollowList follow={follower} key={follower.id}/>
+      })
+
+      
+
+    return (
+        <div>
+            Followers list
+            {followerList}
+        </div>
+    )
+}
+
+export default Followers
